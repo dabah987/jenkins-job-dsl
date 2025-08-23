60990b5c4ec26ca25599590e66384c0efd0fe110
Amir D <amird@Amirs-MacBook-Air.local>
2025-08-23 17:22:55 +0300

    readme2

--- jobs.groovy
+++ jobs.groovy
@@ -39,14 +39,14 @@ CMD python app.py'''
                         }
                         stage('Build Docker Image') {
                             steps {
-                                sh 'docker build -t macen9/flask-app:latest .'
+                                sh 'docker build -t your_dockerhub/flask-app:latest .'
                             }
                         }
                         stage('Push to DockerHub') {
                             steps {
                                 withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
                                     sh 'echo \$PASS | docker login -u \$USER --password-stdin'
-                                    sh 'docker push macen9/flask-app:latest'
+                                    sh 'docker push your_dockerhub/flask-app:latest'
                                 }
                             }
                         }
@@ -81,14 +81,14 @@ COPY default.conf /etc/nginx/conf.d/default.conf'''
                         }
                         stage('Build Docker Image') {
                             steps {
-                                sh 'docker build -t macen9/nginx-proxy:latest .'
+                                sh 'docker build -t your_dockerhub/nginx-proxy:latest .'
                             }
                         }
                         stage('Push to DockerHub') {
                             steps {
                                 withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
                                     sh 'echo \$PASS | docker login -u \$USER --password-stdin'
-                                    sh 'docker push macen9/nginx-proxy:latest'
+                                    sh 'docker push your_dockerhub/nginx-proxy:latest'
                                 }
                             }
                         }
@@ -111,8 +111,8 @@ pipelineJob('deploy_and_test') {
                         stage('Run Containers') {
                             steps {
                                 sh 'docker network create testnet || true'
-                                sh 'docker run -d --rm --name flask-app --network testnet -v /var/run/docker.sock:/var/run/docker.sock macen9/flask-app:latest'
-                                sh 'docker run -d --rm -p 8082:80 --name nginx-proxy --network testnet macen9/nginx-proxy:latest'
+                                sh 'docker run -d --rm --name flask-app --network testnet -v /var/run/docker.sock:/var/run/docker.sock your_dockerhub/flask-app:latest'
+                                sh 'docker run -d --rm -p 8082:80 --name nginx-proxy --network testnet your_dockerhub/nginx-proxy:latest'
                             }
                         }
                         stage('Test Request') {
